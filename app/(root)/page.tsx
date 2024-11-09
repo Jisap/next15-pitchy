fa1158e39526f8c2214b6149458c3e7f9cedec83
Jorge Matos <jisapjisap@gmail.com>
2024-11-09 16:45:41 +0100

    SearchForm

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
-import Image from "next/image";
+
+import SearchForm from "../components/SearchForm";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <p className="sub-heading !max-w-3xl">
         Submit Ideas, Vote on Pitches, and Get Noticed in virtual competitions.
       </p>
+      <SearchForm />
     </section>
     </>
   );
