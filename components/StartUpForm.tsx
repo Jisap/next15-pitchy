0cc1934daaa2a752353e9e8c343156f1b425ec8f
Jorge Matos <jisapjisap@gmail.com>
2024-11-18 18:08:16 +0100

    startupform

--- components/StartUpForm.tsx
+++ components/StartUpForm.tsx
@@ -3,10 +3,18 @@
 import { useState } from "react";
 import { Input } from "./ui/input"
 import { Textarea } from "./ui/textarea";
+import { useToast } from "@/hooks/use-toast";
+import { useRouter } from "next/navigation";
+import MDEditor from "@uiw/react-md-editor";
+import { Button } from "./ui/button";
+import { Send } from "lucide-react";
 
 const StartUpForm = () => {
 
     const [errors, setErrors] = useState<Record<string, string>>({});
+    const [pitch, setPitch] = useState("");
+    const { toast } = useToast();
+    const router = useRouter();
 
   return (
     <form 
@@ -73,6 +81,37 @@ const StartUpForm = () => {
 
         {errors.link && <p className="startup-form_error">{errors.link}</p>}
       </div>
+
+      <div data-color-mode="light">
+        <label htmlFor="pitch" className="startup-form_label">
+          Pitch
+        </label>
+
+        <MDEditor
+          value={pitch}
+          onChange={(value) => setPitch(value as string)}
+          id="pitch"
+          preview="edit"
+          height={300}
+          style={{ borderRadius: 20, overflow: "hidden" }}
+          textareaProps={{
+            placeholder:
+              "Briefly describe your idea and what problem it solves",
+          }}
+          previewOptions={{
+            disallowedElements: ["style"],
+          }}
+        />
+
+        {errors.pitch && <p className="startup-form_error">{errors.pitch}</p>}
+      </div>
+
+      <Button
+        type="submit"
+        className="startup-form_btn text-white"  
+      >  
+        <Send className="size-6 ml-2" />
+      </Button>
     </form>
   )
 }
