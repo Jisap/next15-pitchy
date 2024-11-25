1fe208c1550cdfd3ecf673d587456e5939992285
Jorge Matos <jisapjisap@gmail.com>
2024-11-25 17:18:22 +0100

    PromiseAll

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -15,14 +15,17 @@ export const experimental_ppr = true;
 
 const Page = async({ params }: { params: Promise<{ id: string }> }) => {
 
-  const id = (await params).id
+  const id = (await params).id;
 
-  const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
+  const [post , { select: editorPosts }] = await Promise.all([
+    client.fetch(STARTUP_BY_ID_QUERY, { id }),
+    client.fetch(PLAYLIST_BY_SLUG_QUERY, { slug: "editor-picks" })
+  ])
+
+
   if(!post) return <h1>No startup found</h1>
 
   const parsedContent = md.render(post?.pitch || ""); // Markdown to HTML
-
-  const { select: editorPosts } = await client.fetch(PLAYLIST_BY_SLUG_QUERY, {slug: "editor-picks"})
 
   return (
     <>
